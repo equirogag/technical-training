4b488e4681d535ec4cdfaab2b96ffc126942b3aa
equirogag <equiroga@addval.cl>
2024-09-09 15:37:54 -0300

    edit views names, add active, state and set some defaults fields

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -7,10 +7,10 @@ class EstateProperty(models.Model):
     name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
-    date_availability = fields.Date()
+    date_availability = fields.Date(default='default_date_availability', copy=False)
     expected_price = fields.Float(required=True)
-    selling_price = fields.Float()
-    bedrooms = fields.Integer()
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
     living_area = fields.Integer()
     facades = fields.Integer()
     garage = fields.Boolean()
@@ -19,3 +19,13 @@ class EstateProperty(models.Model):
     garden_orientation = fields.Selection(
         selection = [('north', 'North'),('south','South'),('east','East'), ('west','West')]
     )
+    active= fields.Boolean(default=True)
+    state= fields.Selection(
+        selection = [('new', 'New'), ('offer_received', 'Offer Received'), ('offer_accepted', 'Offer Accepted'), ('sold', 'Sold'), ('cancelled', 'Cancelled')],
+        default='new',
+        required=True,
+        copy=False
+    )
+
+    def default_date_availability(self):
+        self.date_availability.add(fields.Date.today, months=3)
