e0c622eb2a7ca11c4a3e5d84594d65be42264f01
equirogag <equiroga@addval.cl>
2024-09-09 20:02:56 -0300

    testing onchange instead depends

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -40,7 +40,7 @@ class EstateProperty(models.Model):
 
     total_area = fields.Integer(readonly=True, string="Total Area (sqm)", compute="_compute_total_area")
 
-    @api.depends("living_area", "garden_area")
+    @api.onchange("living_area", "garden_area")
     def _compute_total_area(self):
         for record in self:
             record.total_area = record.living_area + record.garden_area
