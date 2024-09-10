3bcb5d2ecf93b1bf336b7eac9ed252958282b318
equirogag <equiroga@addval.cl>
2024-09-10 14:52:35 -0300

    add sequence to estate property type

--- estate/models/estate_property_type.py
+++ estate/models/estate_property_type.py
@@ -3,10 +3,11 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
     _description = "Property types"
-    _order = "name"
+    _order = "sequence, name"
 
     name = fields.Char(required=True)
     property_ids = fields.One2many('estate.property', 'property_type_id', string="Properties")
+    sequence = fields.Integer(string="Sequence", default=1)
 
     _sql_constraints = [
         ('name', 'UNIQUE(name)', 'The name must be unique')
