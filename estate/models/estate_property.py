7292b810f11a3112c1d85ac52a42ef96c81e362d
equirogag <equiroga@addval.cl>
2024-09-10 19:43:17 -0300

    add argument to ondelete decorator

--- estate/models/estate_property.py
+++ estate/models/estate_property.py
@@ -97,8 +97,8 @@ class EstateProperty(models.Model):
             self.state = 'sold'
             return True
 
-    @api.ondelete
-    def _ondelete_property(self, at_uninstall=False):
+    @api.ondelete(at_uninstall=False)
+    def _ondelete_property(self):
         for record in self:
             if record.state != 'new' and record.state != 'canceled':
                 raise UserError("You only can delete properties in New and Canceled states")
