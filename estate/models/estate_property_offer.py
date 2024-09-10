0d694c89b50fea332a83838978ce2a53ae3254ed
equirogag <equiroga@addval.cl>
2024-09-10 11:28:46 -0300

    compute and inverse function to deadline

--- estate/models/estate_property_offer.py
+++ estate/models/estate_property_offer.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, api
 
 class EstatePropertyOffer(models.Model):
     _name="estate.property.offer"
@@ -12,3 +12,15 @@ class EstatePropertyOffer(models.Model):
 
     partner_id = fields.Many2one('res.partner', string="Partner", required=True)
     property_id = fields.Many2one('estate.property', string="Property", required=True)
+
+    validity = fields.Integer(string="Validity(days)", default =7)
+    date_deadline = fields.Date(string="Deadline", compute="_compute_deadline_from_validity", inverse='_compute_validity_from_deadline')
+
+    @api.depends("validity")
+    def _compute_deadline_from_validity(self):
+        for record in self:
+            record.date_deadline = fields.Date.add(fields.Date.today(), days=record.validity)
+
+    def _compute_validity_from_deadline(self):
+        for record in self:
+            record.validity = (record.date_deadline - fields.Date.today()).days
